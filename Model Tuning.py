189f6b8fa2d71c33ba7b26cedd438ca6a5c5ba7d
lmisselyn <45001744+lmisselyn@users.noreply.github.com>
2023-04-10 19:55:50 +0200

    first

---
+++ Model Tuning.py
@@ -0,0 +1,152 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+"Machine learning tools"
+from sklearn import ensemble
+from sklearn import neural_network
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import normalize, Normalizer, StandardScaler
+import xgboost as xgb
+import pickle
+
+from utils_copy import *
+
+dataset_name = 'PandasDataframes/Recorded_Melspecs_16.csv'
+
+data = pd.read_csv(dataset_name).drop(['Unnamed: 0'], axis='columns').drop(index=779)
+
+X_train, X_test, y_train, y_test = train_test_split(data.drop(columns='LABEL')**(1/2), data.LABEL, test_size=0.1)
+
+
+n_neighbors = 3
+
+class_weights = [1,1,1,1,1]
+#class_weights = [2.24,12.68,4.34,4.34,4.34] #weights adjusted manually
+
+run_MLP = True
+
+weights_list = [class_weights[int(i)] for i in y_train]
+
+my_Normalizer = Normalizer() #Normalizer(norm='l2')
+
+def my_kfold(X, y, model='xgb', display=False, n_splits=5) :
+    my_kf = StratifiedKFold(n_splits=n_splits, shuffle=True)
+    kf_scores = []
+    for i, (train_index, test_index) in enumerate(my_kf.split(X, y)):
+        weights_list = [class_weights[int(i)] for i in y.iloc[train_index]]
+        if model == 'xgb' :
+            XGB_model.fit(X.iloc[train_index], y.iloc[train_index], **{'XGBClassifier__sample_weight': weights_list})
+            y_pred = XGB_model.predict(X.iloc[test_index])
+        elif model == 'knn' :
+            knn_model.fit(X.iloc[train_index], y.iloc[train_index])
+            y_pred = knn_model.predict(X.iloc[test_index])
+        elif model == 'mlp' :
+            MLP_model.fit(X.iloc[train_index], y.iloc[train_index])
+            y_pred = MLP_model.predict(X.iloc[test_index])
+        kf_scores.append(get_accuracy(y_pred, y.iloc[test_index]))
+    if display : print(f'K-fold accuracy : {sum(kf_scores)/len(kf_scores)}')
+    return sum(kf_scores)/len(kf_scores)
+
+#TRAINING A KNN CLASSIFIER
+#-----------------------------------------------------------------------------------------------------------
+
+my_KNN = KNeighborsClassifier(n_neighbors=n_neighbors)
+
+#TRAINING A XGB CLASSIFIER
+#-----------------------------------------------------------------------------------------------------------
+
+my_XGB = xgb.sklearn.XGBClassifier()
+#XGB_model.fit(X_train, y_train, **{'XGBClassifier__sample_weight': weights_list})
+
+#TRAINING A MLP CLASSIFIER
+#-----------------------------------------------------------------------------------------------------------
+
+my_MLP = neural_network.MLPClassifier(max_iter=200, hidden_layer_sizes=(500,500))
+
+#PART 1 : PCA DIMENSION OPTIMIZATION
+#---------------------------------------------------------------------------------------------------------------------
+
+all_n_pca = [8,16,24,32,40,48,56,64,128]
+
+knn_scores = []
+xgb_scores = []
+mlp_scores = []
+
+with open('PCALog.txt', 'w') as file :
+    file.write('Recording Different PCA scores with default parameter tuning \n\r')
+    for n_pca in all_n_pca :
+        file.write(f'\n Scores for PCA size {n_pca} :\n\r')
+        my_pca = PCA(n_components=n_pca)
+        knn_model = Pipeline([('NORM', my_Normalizer), ('PCA', my_pca), ('KNNClassifier', my_KNN)])
+        knn_score = my_kfold(X_train, y_train, model='knn', n_splits=2)
+        knn_scores.append(knn_score)
+        file.write(f'Score for KNN : {knn_score} \n\r')
+        XGB_model = Pipeline([('NORM', my_Normalizer), ('PCA', my_pca), ('XGBClassifier', my_XGB)])
+        xgb_score = my_kfold(X_train, y_train, model='xgb', n_splits=2)
+        xgb_scores.append(xgb_score)
+        file.write(f'Score for XGB : {xgb_score} \n\r')
+        MLP_model = Pipeline([('NORM', my_Normalizer), ('PCA', my_pca), ('MLPClassifier', my_MLP)])
+        mlp_score = my_kfold(X_train, y_train, model='mlp', n_splits=2)
+        mlp_scores.append(mlp_score)
+        file.write(f'Score for MLP : {mlp_score} \n\r')
+
+best_knn_pca = all_n_pca[np.argmax(np.array(knn_scores))]
+best_xgb_pca = all_n_pca[np.argmax(np.array(xgb_scores))]
+best_MLP_pca = all_n_pca[np.argmax(np.array(mlp_scores))]
+
+knn_X_train = Pipeline([('NORM', my_Normalizer), ('PCA', PCA(n_components=best_knn_pca))]).fit_transform(X_train)
+xgb_X_train = Pipeline([('NORM', my_Normalizer), ('PCA', PCA(n_components=best_xgb_pca))]).fit_transform(X_train)
+mlp_X_train = Pipeline([('NORM', my_Normalizer), ('PCA', PCA(n_components=best_MLP_pca))]).fit_transform(X_train)
+
+#PART 2 : HYPERPARAMETER OPTIMIZATION
+#---------------------------------------------------------------------------------------------------------------------
+#PART 2.1 : KNN
+
+KNN_params = {'n_neighbors' : list(range(3)),
+              'weights' : ['uniform', 'distance'],
+              'p' : [1,2,3]}
+
+KNN_GS = GridSearchCV(KNeighborsClassifier(), KNN_params, n_jobs=-1, cv=10, scoring='accuracy')
+KNN_GS.fit(knn_X_train, y_train) # X is train samples and y is the corresponding labels
+
+#PART 2.2 : XGB
+
+XGB_params = {
+        'max_depth': [2,3,4,5,6,7,8,9,10,15,20,25,30,50],
+        'n_estimators' : [100,200,300,500],
+        'min_child_weight' : [0,0.5,1,2]
+    }
+
+XGB_GS = GridSearchCV(xgb.XGBClassifier(),
+                      XGB_params, n_jobs=-1, cv=10, scoring='accuracy')
+XGB_GS.fit(xgb_X_train, y_train)
+
+#PART 2.3 : MLP
+
+MLP_params = {
+        'hidden_layer_sizes': [(100,),(1000,),(100,100),(1000,1000),(20,20,20),(64,128,64)],
+        'solver': ['sgd', 'adam'],
+        'alpha': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1],
+        'momentum' : [0,0.7,0.8,0.9,0.95],
+        'learning_rate': ['constant','adaptive']
+    }
+
+MLP_GS = GridSearchCV(neural_network.MLPClassifier(max_iter=500, nesterovs_momentum=True ,
+                                                   activation='relu', early_stopping=True,
+                                                   n_iter_no_change=50),
+                      MLP_params, n_jobs=-1, cv=10, scoring='accuracy')
+MLP_GS.fit(mlp_X_train, y_train)
+
+with open('GridSearchLog.txt', 'w') as file :
+    file.write(f'Results for grid searches on all selected models \n\r')
+    file.write(f'Best set of parameters for KNN model : {KNN_GS.best_params_} gave score {KNN_GS.best_score_}\n\r')
+    file.write(f'Best set of parameters for XGB model : {XGB_GS.best_params_} gave score {XGB_GS.best_score_}\n\r')
+    file.write(f'Best set of parameters for KNN model : {MLP_GS.best_params_} gave score {MLP_GS.best_score_}\n\r')
---
+++ utils_copy.py
@@ -0,0 +1,235 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+"For confusion matrix plot"
+from seaborn import heatmap
+from sklearn.metrics import confusion_matrix
+
+# ----------------------------------------------------------------------------------
+"""
+Synthesis of the functions in :
+- getclass : Get class name of a sound path directory.
+- getname : Get name of a sound from its path directory.
+- gen_allpath : Generate a matrix with path names and an array with all class names.
+- plot_audio : Plot an audiosignal in time and frequency.
+- plot_specgram : Plot a spectrogram (2D matrix).
+- get_accuracy : Compute the accuracy between prediction and ground truth.
+- show_confusion_matrix : Plot confusion matrix.
+- plot_decision_boundary : Plot decision boundary of a classifier.
+"""
+# ----------------------------------------------------------------------------------
+
+
+def getclass(sound, format=".ogg"):
+    """Get class name of a sound path directory.
+    Note: this function is only compatible with ESC-50 dataset path organization.
+    """
+    L = len(format)
+
+    folders = sound.split(os.path.sep)
+    if folders[-1][-L:] == format:
+        return folders[-2].split("-")[1]
+    else:
+        return folders[-1].split("-")[1]
+
+
+def getname(sound):
+    """
+    Get name of a sound from its path directory.
+    """
+    return os.path.sep.join(sound.split(os.path.sep)[-2:])
+
+
+def gen_allpath(folder=r"Dataset_ESC-50"):
+    """
+    Create a matrix with path names of height H=50classes and W=40sounds per class
+    and an array with all class names.
+    """
+    classpath = [f.path for f in os.scandir(folder) if f.is_dir()]
+    classpath = sorted(classpath)
+
+    allpath = [None] * len(classpath)
+    classnames = [None] * len(classpath)
+    for ind, val in enumerate(classpath):
+        classnames[ind] = getclass(val).strip()
+        sublist = [None] * len([f.path for f in os.scandir(val)])
+        for i, f in enumerate(os.scandir(val)):
+            sublist[i] = f.path
+        allpath[ind] = sublist
+
+    allpath = np.array(allpath)
+    return classnames, allpath
+
+
+def plot_audio(audio, audio_down, fs, fs_down):
+    """
+    Plot the temporal and spectral representations of the original audio signal and its downsampled version
+    """
+    M = fs // fs_down  # Downsampling factor
+
+    L = len(audio)
+    L_down = len(audio_down)
+
+    frequencies = np.arange(-L // 2, L // 2, dtype=np.float64) * fs / L
+    frequencies_down = (
+        np.arange(-L_down // 2, L_down // 2, dtype=np.float64) * fs_down / L_down
+    )
+    spectrum = np.fft.fftshift(np.fft.fft(audio))
+    spectrum_down = np.fft.fftshift(np.fft.fft(audio_down))
+
+    fig = plt.figure(figsize=(12, 4))
+    ax1 = fig.add_axes([0.0, 0.0, 0.42, 0.9])
+    ax2 = fig.add_axes([0.54, 0.0, 0.42, 0.9])
+
+    ax1.plot(np.arange(L) / fs, audio, "b", label="Original")
+    ax1.plot(np.arange(L_down) / fs_down, audio_down, "r", label="Downsampled")
+    ax1.legend()
+    ax1.set_xlabel("Time [s]")
+    ax1.set_ylabel("Amplitude [-]")
+    ax1.set_title("Temporal signal")
+
+    ax2.plot(frequencies, np.abs(spectrum), "b", label="Original")
+    ax2.plot(
+        frequencies_down, np.abs(spectrum_down) * M, "r", label="Downsampled", alpha=0.5
+    )  # energy scaling by M
+    ax2.legend()
+    ax2.set_xlabel("Frequency [Hz]")
+    ax2.set_ylabel("Amplitude [-]")
+    ax2.set_title("Modulus of FFT")
+    plt.show()
+
+
+def plot_specgram(
+    specgram,
+    ax,
+    is_mel=False,
+    title=None,
+    xlabel="Time [s]",
+    ylabel="Frequency [Hz]",
+    cmap="jet",
+    cb=True,
+    tf=None,
+):
+    """Plot a spectrogram (2D matrix) in a chosen axis of a figure.
+    Inputs:
+        - specgram = spectrogram (2D array)
+        - ax       = current axis in figure
+        - title
+        - xlabel
+        - ylabel
+        - cmap
+        - cb       = show colorbar if True
+        - tf       = final time in xaxis of specgram
+    """
+    if tf is None:
+        tf = specgram.shape[1]
+
+    if is_mel:
+        ylabel = "Frequency [Mel]"
+        im = ax.imshow(
+            specgram,
+            cmap=cmap,
+            aspect="auto",
+            extent=[0, tf, specgram.shape[0], 0],
+            origin="lower",
+        )
+    else:
+        im = ax.imshow(
+            specgram,
+            cmap=cmap,
+            aspect="auto",
+            extent=[0, tf, int(specgram.size / tf), 0],
+            origin="lower",
+        )
+    fig = plt.gcf()
+    if cb:
+        cbar = fig.colorbar(im, ax=ax)
+    # cbar.set_label('log scale', rotation=270)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    return None
+
+
+def get_accuracy(prediction, target):
+    """
+    Compute the accuracy between prediction and ground truth.
+    """
+
+    return np.sum(prediction == target) / len(prediction)
+def get_precision( prediction , target, classname):
+    """
+    Compute the precision between prediction and ground truth.
+    """
+    TP = 0
+    FP = 0
+    for i in range(len(prediction)):
+        if prediction[i] == classname and target[i] == classname:
+            TP += 1
+        elif prediction[i] == classname and target[i] != classname:
+            FP += 1
+    return TP / (TP + FP)
+
+def get_recall(prediction, target, classname):
+    """
+    Compute the recall between prediction and ground truth.
+    """
+    TP = 0
+    FN = 0
+    for i in range(len(prediction)):
+        if prediction[i] == classname and target[i] == classname:
+            TP += 1
+        elif prediction[i] != classname and target[i] == classname:
+            FN += 1
+    return TP / (TP + FN)
+    
+def get_f_score(prediction, target, classname):
+    return 2/(1/get_recall(prediction, target, classname)+1/get_precision(prediction, target, classname))
+
+def show_confusion_matrix(y_predict, y_true2, classnames, title=""):
+    """
+    From target and prediction arrays, plot confusion matrix.
+    The arrays must contain ints.
+    """
+
+    confmat = confusion_matrix(
+        y_true2, y_predict, labels=np.arange(np.max(y_true2) + 1)
+    )
+    heatmap(
+        confmat.T,
+        square=True,
+        annot=True,
+        fmt="d",
+        cbar=False,
+        xticklabels=classnames,
+        yticklabels=classnames,
+    )
+    plt.xlabel("True label")
+    plt.ylabel("Predicted label")
+    plt.title(title)
+    plt.savefig("confusion_matrix.svg")
+    plt.show()
+    return None
+
+def plot_decision_boundaries(X,y,model, ax=None, legend='',title='', s=20, N=40, cm='brg', edgc='k'):
+    """
+    Plot decision boundaries of a classifier in 2D, and display true labels.
+    """
+    if ax is None:
+        fig = plt.figure(figsize=(4,4))
+        ax = fig.add_axes([0.0,0.0,0.9,1.0]) 
+    ax.set_aspect('equal', adjustable='box')
+    # Plot the decision boundary. 
+    n = 80
+    vec = np.linspace(np.min(X),np.max(X),n)
+    Xtmp = np.meshgrid(vec, vec)
+    Xtmp2 = np.array(Xtmp).reshape(2,n**2).T
+    ax.contourf(Xtmp[0], Xtmp[1], model.predict(Xtmp2).reshape(n,n), cmap=cm, alpha=0.5)
+    scatterd = ax.scatter(X[:,0],X[:,1], c=y, cmap=cm, edgecolors=edgc, s=s)
+    ax.set_title(title)
+    ax.set_xlabel('$x_1$')
+    ax.set_ylabel('$x_2$')
+    handles, labels = scatterd.legend_elements(prop="colors")
+    ax.legend(handles, legend)
