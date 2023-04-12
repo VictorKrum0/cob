02ac954d00170639b5f587d2a51e96b31cebf391
VictorKrum0 <76691452+VictorKrum0@users.noreply.github.com>
2023-04-12 12:08:31 +0200

    Update Model Tuning.py

--- Model Tuning.py
+++ Model Tuning.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import warnings
 
 "Machine learning tools"
 from sklearn import ensemble
@@ -19,11 +20,13 @@ import pickle
 
 from utils_copy import *
 
+warnings.filterwarnings("ignore")
+
 dataset_name = 'PandasDataframes/Recorded_Melspecs_16.csv'
 
 data = pd.read_csv(dataset_name).drop(['Unnamed: 0'], axis='columns').drop(index=779)
 
-X_train, X_test, y_train, y_test = train_test_split(data.drop(columns='LABEL')**(1/2), data.LABEL, test_size=0.1)
+X_train, X_test, y_train, y_test = train_test_split(data.drop(columns='LABEL')**(1/2), data.LABEL, test_size=0.01)
 
 
 n_neighbors = 3
@@ -86,21 +89,23 @@ with open('PCALog.txt', 'w') as file :
         file.write(f'\n Scores for PCA size {n_pca} :\n\r')
         my_pca = PCA(n_components=n_pca)
         knn_model = Pipeline([('NORM', my_Normalizer), ('PCA', my_pca), ('KNNClassifier', my_KNN)])
-        knn_score = my_kfold(X_train, y_train, model='knn', n_splits=2)
+        knn_score = my_kfold(X_train, y_train, model='knn', n_splits=20)
         knn_scores.append(knn_score)
         file.write(f'Score for KNN : {knn_score} \n\r')
         XGB_model = Pipeline([('NORM', my_Normalizer), ('PCA', my_pca), ('XGBClassifier', my_XGB)])
-        xgb_score = my_kfold(X_train, y_train, model='xgb', n_splits=2)
+        xgb_score = my_kfold(X_train, y_train, model='xgb', n_splits=20)
         xgb_scores.append(xgb_score)
         file.write(f'Score for XGB : {xgb_score} \n\r')
         MLP_model = Pipeline([('NORM', my_Normalizer), ('PCA', my_pca), ('MLPClassifier', my_MLP)])
-        mlp_score = my_kfold(X_train, y_train, model='mlp', n_splits=2)
+        mlp_score = my_kfold(X_train, y_train, model='mlp', n_splits=20)
         mlp_scores.append(mlp_score)
         file.write(f'Score for MLP : {mlp_score} \n\r')
-
-best_knn_pca = all_n_pca[np.argmax(np.array(knn_scores))]
-best_xgb_pca = all_n_pca[np.argmax(np.array(xgb_scores))]
-best_MLP_pca = all_n_pca[np.argmax(np.array(mlp_scores))]
+    best_knn_pca = all_n_pca[np.argmax(np.array(knn_scores))]
+    best_xgb_pca = all_n_pca[np.argmax(np.array(xgb_scores))]
+    best_MLP_pca = all_n_pca[np.argmax(np.array(mlp_scores))]  
+    file.write(f'\n\n\rBest PCA for KNN : {best_knn_pca}\n\r')
+    file.write(f'Best PCA for KNN : {best_xgb_pca}\n\r')
+    file.write(f'Best PCA for MLP : {best_MLP_pca}\n\r')
 
 knn_X_train = Pipeline([('NORM', my_Normalizer), ('PCA', PCA(n_components=best_knn_pca))]).fit_transform(X_train)
 xgb_X_train = Pipeline([('NORM', my_Normalizer), ('PCA', PCA(n_components=best_xgb_pca))]).fit_transform(X_train)
@@ -110,25 +115,25 @@ mlp_X_train = Pipeline([('NORM', my_Normalizer), ('PCA', PCA(n_components=best_M
 #---------------------------------------------------------------------------------------------------------------------
 #PART 2.1 : KNN
 
-KNN_params = {'n_neighbors' : list(range(3)),
+KNN_params = {'n_neighbors' : list(range(1,20)),
               'weights' : ['uniform', 'distance'],
-              'p' : [1,2,3]}
+              'p' : [1,2,3,4]}
 
 KNN_GS = GridSearchCV(KNeighborsClassifier(), KNN_params, n_jobs=-1, cv=10, scoring='accuracy')
-KNN_GS.fit(knn_X_train, y_train) # X is train samples and y is the corresponding labels
+KNN_GS.fit(knn_X_train, y_train)
 
 #PART 2.2 : XGB
 
 XGB_params = {
-        'max_depth': [2,3,4,5,6,7,8,9,10,15,20,25,30,50],
-        'n_estimators' : [100,200,300,500],
-        'min_child_weight' : [0,0.5,1,2]
+        'max_depth': [10,13,14,15,16,17,18,19,20,21,22,23,25,30,50],
+        'n_estimators' : [800,900,1000,1100,1200,1500,2000],
+        'min_child_weight' : [0,0.5,1]
     }
 
 XGB_GS = GridSearchCV(xgb.XGBClassifier(),
                       XGB_params, n_jobs=-1, cv=10, scoring='accuracy')
 XGB_GS.fit(xgb_X_train, y_train)
-
+'''
 #PART 2.3 : MLP
 
 MLP_params = {
@@ -144,9 +149,9 @@ MLP_GS = GridSearchCV(neural_network.MLPClassifier(max_iter=500, nesterovs_momen
                                                    n_iter_no_change=50),
                       MLP_params, n_jobs=-1, cv=10, scoring='accuracy')
 MLP_GS.fit(mlp_X_train, y_train)
-
+'''
 with open('GridSearchLog.txt', 'w') as file :
     file.write(f'Results for grid searches on all selected models \n\r')
     file.write(f'Best set of parameters for KNN model : {KNN_GS.best_params_} gave score {KNN_GS.best_score_}\n\r')
     file.write(f'Best set of parameters for XGB model : {XGB_GS.best_params_} gave score {XGB_GS.best_score_}\n\r')
-    file.write(f'Best set of parameters for KNN model : {MLP_GS.best_params_} gave score {MLP_GS.best_score_}\n\r')
+    #file.write(f'Best set of parameters for KNN model : {MLP_GS.best_params_} gave score {MLP_GS.best_score_}\n\r')
